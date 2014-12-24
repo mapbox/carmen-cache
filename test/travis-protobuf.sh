ce7b3bff02df04c7b699ff7929e199c7009b1613
Dane Springmeyer <dane@mapbox.com>
2014-12-23 18:27:18 -0800

    use protobuf 2.6.1

--- test/travis-protobuf.sh
+++ test/travis-protobuf.sh
@@ -7,10 +7,11 @@ build_dir="$(pwd)"
 export PATH="/usr/local/bin:$PATH"
 export CXXFLAGS="$CXXFLAGS -fPIC"
 
-wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.gz -O /tmp/protobuf-2.5.0.tar.gz
-tar xf /tmp/protobuf-2.5.0.tar.gz -C /tmp
-cd /tmp/protobuf-2.5.0
+PROTOBUF_VERSION=2.6.1
+wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.bz2 -O /tmp/protobuf-${PROTOBUF_VERSION}.tar.bz2
+tar xf /tmp/protobuf-${PROTOBUF_VERSION}.tar.bz2 -C /tmp
+cd /tmp/protobuf-${PROTOBUF_VERSION}
 ./configure --enable-static --disable-shared --disable-dependency-tracking
-make
+make -j8
 make install
 cd $build_dir
