d64c56cb6c8d29d21f4eb4232153c4a5bc808dda
Young Hahn <young@mapbox.com>
2015-01-18 12:17:08 -0500

    Add (failing) test for querydist mapping when degenerate term is found.

--- test/phrasematchDegens.test.js
+++ test/phrasematchDegens.test.js
@@ -19,4 +19,21 @@ tape('#phrasematchDegens', function(assert) {
     });
 });
 
+tape('#phrasematchDegens + dist', function(assert) {
+    var cache = new Cache('a', 1);
+    cache.phrasematchDegens([
+        [ 681154065 ],
+        [ 3790501696 ]
+    ], function(err, result) {
+        assert.ifError(err);
+        assert.deepEqual(result, {
+            terms: [ 681154064, 3790501696 ],
+            queryidx: { '681154064': 0, '3790501696': 1 },
+            querymask: { '681154064': 1, '3790501696': 2 },
+            querydist: { '681154064': 1, '3790501696': 0 }
+        });
+        assert.end();
+    });
+});
+
 
