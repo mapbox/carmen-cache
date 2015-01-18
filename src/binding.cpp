6089503393a0bf92a6a96ddf8d3d9648b4c4dfb0
Young Hahn <young@mapbox.com>
2015-01-18 12:17:19 -0500

    Fix querydist generation

--- src/binding.cpp
+++ src/binding.cpp
@@ -730,7 +730,7 @@ void _phrasematchDegens(uv_work_t* req) {
 
             it = querydist.find(term);
             if (it == querydist.end()) {
-                querydist.emplace(term, term % 16);
+                querydist.emplace(term, degens[i] % 16);
             }
         }
     }
