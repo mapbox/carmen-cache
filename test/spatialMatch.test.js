dad54b4a895699b249af474a72d94cec5ea53104
Young Hahn <young@mapbox.com>
2015-03-10 11:25:56 -0400

    More readable and add a test for multiple tied top features.

--- test/spatialMatch.test.js
+++ test/spatialMatch.test.js
@@ -71,8 +71,8 @@ test('unit', function(assert) {
             Cover.encode({ x: 33, y: 32, id: 1 })
         ],
         [
-            Cover.encode({ x: 33, y: 32, id: 2 }),
-            Cover.encode({ x: 32, y: 32, id: 1 })
+            Cover.encode({ x: 32, y: 32, id: 1 }),
+            Cover.encode({ x: 33, y: 32, id: 2 })
         ],
         [
             Cover.encode({ x: 32, y: 32, id: 1 }),
@@ -84,61 +84,13 @@ test('unit', function(assert) {
     spatialMatch(queryLength, feat, cover, zooms, function(err, ret) {
         assert.ifError(err);
         assert.deepEqual(ret.sets, {
-            1: Relev.encode({
-                id: 1,
-                idx: 0,
-                tmpid: 1,
-                reason: 8,
-                count: 1,
-                relev: 1,
-                check: true
-            }),
-            100000001: Relev.encode({
-                id: 1,
-                idx: 1,
-                tmpid: 100000001,
-                reason: 8,
-                count: 1,
-                relev: 1,
-                check: true
-            }),
-            100000002: Relev.encode({
-                id: 2,
-                idx: 1,
-                tmpid: 100000002,
-                reason: 4,
-                count: 1,
-                relev: 1,
-                check: true
-            }),
-            200000001: Relev.encode({
-                id: 1,
-                idx: 2,
-                tmpid: 200000001,
-                reason: 3,
-                count: 2,
-                relev: 1,
-                check: true
-            }),
-            200000002: Relev.encode({
-                id: 2,
-                idx: 2,
-                tmpid: 200000002,
-                reason: 3,
-                count: 2,
-                relev: 1,
-                check: true
-            })
+            1: feat['1'],
+            100000001: feat['100000001'],
+            100000002: feat['100000002'],
+            200000001: feat['200000001'],
+            200000002: feat['200000002']
         });
-        assert.deepEqual(ret.results, [ Relev.encode({
-            id: 2,
-            idx: 2,
-            tmpid: 200000002,
-            reason: 3,
-            count: 2,
-            relev: 1,
-            check: true
-        }) ]);
+        assert.deepEqual(ret.results, [ feat['200000002'] ]);
         assert.deepEqual(ret.coalesced, {
             1611137056: [
                 1,
@@ -149,6 +101,67 @@ test('unit', function(assert) {
                 1,
                 100000002,
                 200000002
+            ]
+        });
+        assert.end();
+    });
+});
+
+test('tied-top', function(assert) {
+    var queryLength = 2;
+    var feat = {
+        1: Relev.encode({
+            id: 1,
+            idx: 0,
+            tmpid: 1,
+            reason: 1,
+            count: 1,
+            relev: 1,
+            check: true
+        }),
+        100000001: Relev.encode({
+            id: 1,
+            idx: 1,
+            tmpid: 100000001,
+            reason: 2,
+            count: 1,
+            relev: 1,
+            check: true
+        }),
+        100000002: Relev.encode({
+            id: 2,
+            idx: 1,
+            tmpid: 100000002,
+            reason: 2,
+            count: 1,
+            relev: 1,
+            check: true
+        })
+    };
+    var cover = [
+        [
+            Cover.encode({ x: 32, y: 32, id: 1 })
+        ],
+        [
+            Cover.encode({ x: 32, y: 32, id: 1 }),
+            Cover.encode({ x: 32, y: 32, id: 2 })
+        ],
+    ];
+    var zooms = [6,6];
+
+    spatialMatch(queryLength, feat, cover, zooms, function(err, ret) {
+        assert.ifError(err);
+        assert.deepEqual(ret.sets, {
+            1: feat['1'],
+            100000001: feat['100000001'],
+            100000002: feat['100000002']
+        });
+        assert.deepEqual(ret.results, [ feat['100000001'], feat['100000002'] ]);
+        assert.deepEqual(ret.coalesced, {
+            1611137056: [
+                1,
+                100000001,
+                100000002
             ]
         });
         assert.end();
