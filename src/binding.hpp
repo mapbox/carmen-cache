f7e133e3b24e1365acf6c10fdc9f7c83d00ed3f0
Andrew Pendleton <andrew@mapbox.com>
2016-10-18 19:22:15 -0400

    Remove debug code from header

--- src/binding.hpp
+++ src/binding.hpp
@@ -80,21 +80,4 @@ public:
 
 }
 
-
-#include <stdexcept>
-#include <sstream>
-#include <iomanip>
-#include <string>
-#include <cstdint>
-
-std::string string_to_hex(const std::string& in) {
-    std::stringstream ss;
-
-    ss << std::hex << std::setfill('0');
-    for (size_t i = 0; in.length() > i; ++i) {
-        ss << std::setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(in[i]));
-    }
-
-    return ss.str();
-}
 #endif // __CARMEN_BINDING_HPP__
