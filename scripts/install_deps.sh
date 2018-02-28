766d3a50d55130705ac7020519becd5e39878dee
Dane Springmeyer <dane@mapbox.com>
2018-02-27 17:45:33 -0800

    correctly limit compiler concurrency

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -11,6 +11,10 @@ unset MAKEFLAGS
 ./scripts/setup.sh --config local.env
 source local.env
 
+# avoid mis-reporting of CPU due to docker
+# from resulting in OOM killer knocking out g++
+export MASON_CONCURRENCY=2
+
 # only build from source if it does not exist
 if [[ ! -f mason_packages/.link/lib/libbz2.a ]]; then
     mason build bzip2 1.0.6
@@ -22,6 +26,6 @@ if [[ ! -f mason_packages/.link/lib/librocksdb.a ]]; then
 fi
 
 mason link bzip2 1.0.6
-MASON_CONCURRENCY=2 mason link rocksdb 4.13
+mason link rocksdb 4.13
 mason install protozero 1.5.1
 mason link protozero 1.5.1
