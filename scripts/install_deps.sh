e96f0f88532d90f612fad08257da480afd047443
Jeff Miccolis <jeff@miccolis.net>
2018-11-29 13:53:49 -0500

    Update to Rocksdb 5.4.6

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -17,5 +17,9 @@ export MASON_CONCURRENCY=2
 
 mason install clang++ 3.9.1
 mason install bzip2 1.0.6
-mason install rocksdb 5.3.6
+mason install rocksdb 5.4.6
+mason install protozero 1.6.2
+
+mason link bzip2 1.0.6
 mason link protozero 1.6.2
+mason link rocksdb 5.4.6
