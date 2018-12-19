6862db80a4d253bf84707997f1558a3f668d62a1
Dane Springmeyer <springmeyer@users.noreply.github.com>
2018-12-19 10:25:01 -0800

    Remove rocksdb related build hacks
    These were needed to source compile rocksdb on travis. But we're no longer doing that after https://github.com/mapbox/carmen-cache/commit/14f138ebd31ccd03bc7b38acc2738321d93d2a55#diff-1d889b31bb06cb578abde8e2253d043aL6 (which is awesome) so these things can be removed to avoid any confusion.

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,17 +3,9 @@
 set -eu
 set -o pipefail
 
-# gyp will put "MAKEFLAGS=r -- BUILDTYPE=Release" into the makefiles
-# which breaks the rocksdb build
-unset MAKEFLAGS
-
 # setup mason
 ./scripts/setup.sh --config local.env
 source local.env
-
-# avoid mis-reporting of CPU due to docker
-# from resulting in OOM killer knocking out g++
-export MASON_CONCURRENCY=2
 
 mason install clang++ 3.9.1
 mason install bzip2 1.0.6
