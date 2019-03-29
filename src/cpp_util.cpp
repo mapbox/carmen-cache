f6e07cbac1f484ec7fece4d6a4622c8586fa0309
nmargolis <nmargolis89@gmail.com>
2019-03-29 23:54:35 +0000

    make distance floor

--- src/cpp_util.cpp
+++ src/cpp_util.cpp
@@ -156,7 +156,7 @@ double scoredist(unsigned zoom, double distance, unsigned short score, double ra
         1096.6331584284585};
 
     if (distance < 1) {
-        distance = 1;
+        distance = 0.9;
     }
 
     double distRatio = distance / proximityRadius(zoom, radius);
